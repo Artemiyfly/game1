a7ecf58a8ed5387a27fbe0809abefabf76703c27
Freim <k_glebuha@mail.ru>
2017-07-25 11:23:21 +0300

    msg

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
+import random
+import math
 
-print("Nothing")
+white = ( 255, 255, 255)
+
+main_dis_w = 700
+main_dis_h = 400
+
+screen = pygame.display.set_mode(main_dis_w, main_dis_h)
+
