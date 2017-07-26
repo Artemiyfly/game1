bbf2d1ef277f2d368fd634f8f2e013dd820937f5
Freim <k_glebuha@mail.ru>
2017-07-26 12:22:15 +0300

    Сделаль функцию движения перса через WASD

--- graphics.py
+++ graphics.py
@@ -12,7 +12,28 @@ class Window():
 	def redraw():
 		pass
 
-class Menu():
-        def title(title_name ,content, koord):
-                title_name = 
-                
+class Moving():
+        def key_mov:
+                if event.type == pygame.KEYUP:
+                        if event.key = K_UP:
+                                h_y_speed = -3
+                        if event.key = K_DOWN:
+                                h_y_speed = 3
+                        if event.key = K_LEFT:
+                                h_x_speed = -3
+                        if event.key = K_RIGHT:
+                                h_y_speed = 3
+
+                if event.type == pygame.KEYDOWN:
+                        if event.key = K_UP:
+                                h_y_speed = 0
+                        if event.key = K_DOWN:
+                                h_y_speed = 0
+                        if event.key = K_LEFT:
+                                h_x_speed = 0
+                        if event.key = K_RIGHT:
+                                h_y_speed = 0
+
+                h_x_coord = x_coord + h_x_speed
+                h_y_coord = y_coord + h+y_speed
+                        
